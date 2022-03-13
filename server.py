23512d3ebb01655b4eee53ad20c3fb66db17cef9
Prince Agyei-Twum <princeagyeitwum0@gmail.com>
2022-03-13 19:06:50 +0000

    ini first

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ Bootstrap(app)
 socketio = SocketIO(app, manage_session=False)
 app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
 
-app.config["SECRET_KEY"] = os.environ.get("SECRET")
+app.config["SECRET_KEY"] = "SFDDFDD"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 # app.secret_key = "secrete"
