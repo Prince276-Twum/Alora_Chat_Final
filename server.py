334e5aa462f8c9787e7e348c13079c45ebd99c0b
Prince Agyei-Twum <princeagyeitwum0@gmail.com>
2022-03-13 19:18:01 +0000

    ini first

--- server.py
+++ server.py
@@ -14,13 +14,15 @@ import time
 app = Flask(__name__)
 Bootstrap(app)
 socketio = SocketIO(app, manage_session=False)
+
+# app.secret_key=os.environ.get('SECRET')
 app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
 
-app.config["SECRET_KEY"] = "SFDDFDD"
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+# Configure database
+# app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
 
-# app.secret_key = "secrete"
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
+app.secret_key = "secrete"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
