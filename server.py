32de47151d7d6a721c68222077b1f1203239723b
Prince Agyei-Twum <princeagyeitwum0@gmail.com>
2022-03-16 11:38:57 +0000

    ini first

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ import os
 
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get("SECRET")
+# app.config["SECRET_KEY"] = os.environ.get("SECRET")
 app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
 
 
